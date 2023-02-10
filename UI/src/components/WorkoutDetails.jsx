06b71a6162dc5e6a9dd936396146752a13406eff
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-10 11:54:55 -0500

    add delete button and onClick function

--- UI/src/components/WorkoutDetails.jsx
+++ UI/src/components/WorkoutDetails.jsx
@@ -1,4 +1,18 @@
+import axios from 'axios';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleDeleteClick = async () => {
+    const res = await axios.delete('/workouts/' + workout._id);
+
+    if (res.status === 200 && res.data) {
+      dispatch({ type: 'DELETE_WORKOUTS', payload: res.data });
+      console.log('Workout Deleted');
+    }
+  };
+
   return (
     <main className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +25,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleDeleteClick}>delete</span>
     </main>
   );
 };
