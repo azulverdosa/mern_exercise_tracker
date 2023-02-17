1b5ae3c83014cd8e69af8cd9b11008e43d0bc9e4
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-17 17:23:21 -0500

    buils register route

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -8,9 +8,21 @@ const loginUser = async (req, res) => {
 };
 
 //register user
-
 const registerUser = async (req, res) => {
-  res.json({ mssg: 'User registered' });
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.register({
+      name,
+      email,
+      password,
+    });
+    console.log('New User created');
+    return res.status(200).send({ email, user });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).send({ error: err.message });
+  }
 };
 
 module.exports = { loginUser, registerUser };
