1f235719c6c916706ff2eb20d663e04d2a999421
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-17 14:39:18 -0500

    create userController

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -1,16 +1,16 @@
-const user = require('../models/userModel');
+const User = require('../models/userModel');
 
-//ogin user
+//login user
 const loginUser = async (req, res) => {
   // const { name, password } = req.body;
 
-  return res.json({ mssg: 'User logged in' });
+  res.json({ mssg: 'User logged in' });
 };
 
 //register user
 
 const registerUser = async (req, res) => {
-  return res.json({ mssg: 'User registered' });
+  res.json({ mssg: 'User registered' });
 };
 
 module.exports = { loginUser, registerUser };
