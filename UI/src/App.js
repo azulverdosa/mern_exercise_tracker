d016375bfae72946ff85d562bfcb95c2a8a626bf
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-22 12:24:04 -0500

    add register and login paths

--- UI/src/App.js
+++ UI/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NavBar from './components/Navbar';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
