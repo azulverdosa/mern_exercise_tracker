9f9c08522dc8e797847fe400934b501f3c70f98d
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-22 13:12:12 -0500

    remove json and replace with res.data instead

--- UI/src/hooks/useRegister.js
+++ UI/src/hooks/useRegister.js
@@ -7,33 +7,33 @@ export const useRegister = () => {
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
 
-  const register = async (email, password) => {
+  const register = async (name, email, password) => {
     setIsLoading(true);
     setError(null);
 
-    try {
-      const res = await axios.post('/user/register', {
-        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json;charset=UTF-8' },
-        body: JSON.stringify({ email, password }),
-      });
-      const json = await res.json();
+    const user = { name, email, password };
 
-      if (!res.status === 200) {
-        setIsLoading(false);
-        setError(json.error);
-      }
+    try {
+      const res = await axios.post('/user/register', user, {
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json;charset=UTF-8' },
+      });
 
       if (res.status === 200) {
         //save user to local storage
-        localStorage.setItem('user', JSON.stringify(json));
+        localStorage.setItem('user', res.data);
 
         //update AuthCOntext
-        dispatch({ type: 'LOGIN', payload: json });
+        dispatch({ type: 'LOGIN', payload: res.data });
+
+        // NOTE: could res.data be changed to user? - prob not because user password is not hashed with just user
 
         setIsLoading(false);
       }
     } catch (err) {
       console.error('ERROR: ', err);
+
+      setIsLoading(false);
+      setError(err?.response?.data?.error || err?.message || 'something went wrong in the server');
     }
   };
   return { register, isLoading, error };
