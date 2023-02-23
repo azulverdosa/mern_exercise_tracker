b3de7f373c77626c6a86b3a10862ec2219df62c4
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-23 17:00:20 -0500

    import useLogin hook & use , add clearFields func

--- UI/src/pages/Login.jsx
+++ UI/src/pages/Login.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
 
+  const clearFields = () => {
+    setEmail('');
+    setPassword('');
+  };
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    await login(email, password);
+
+    clearFields();
   };
 
   return (
@@ -20,7 +28,8 @@ const Login = () => {
       <label>Password:</label>
       <input type={'password'} value={password} onChange={(e) => setPassword(e.target.value)} />
 
-      <button>Login</button>
+      <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
