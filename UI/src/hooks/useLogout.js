08caef6a2c651cb72d2876cf31e18cf6ac3a99a3
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-23 16:58:26 -0500

    create useLogout hook

---
+++ UI/src/hooks/useLogout.js
@@ -0,0 +1,16 @@
+import { useAuthContext } from './useAuthContext';
+
+//to log out 1) update local state 2) delete JWT from local storage
+export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+
+  const logout = () => {
+    //remove user from storage
+    localStorage.removeItem('user');
+
+    //dispatch logout action
+    dispatch({ type: 'LOGOUT' });
+  };
+
+  return { logout };
+};
