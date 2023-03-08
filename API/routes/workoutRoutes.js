ff12f1a21e48073509b10df7e57091694bbb67d4
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-08 13:47:50 -0500

    require auth on all routes

--- API/routes/workoutRoutes.js
+++ API/routes/workoutRoutes.js
@@ -6,9 +6,13 @@ const {
   editWorkout,
   deleteWorkout,
 } = require('../controllers/workoutController');
+const requireAuth = require('../middleware/requireAuth');
 
 //instance of Router
 const router = express.Router();
+
+//require auth middleware for all workout routes
+router.use(requireAuth);
 
 //GET all workouts
 router.get('/', getAllWorkouts);
