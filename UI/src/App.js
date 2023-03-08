296c00545d83d192a3dfc0d51f66b09230f2299c
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-08 14:23:48 -0500

    conditional navigation based on user

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 import Home from './pages/Home';
 import NavBar from './components/Navbar';
@@ -6,15 +7,17 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
