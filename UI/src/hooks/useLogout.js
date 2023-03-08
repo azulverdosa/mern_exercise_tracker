49666bdc2a035290cc45f36764f0fcd8ae555d89
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-08 15:27:28 -0500

    add dispatch to clear payload on logout

--- UI/src/hooks/useLogout.js
+++ UI/src/hooks/useLogout.js
@@ -1,15 +1,18 @@
 import { useAuthContext } from './useAuthContext';
+import { useWorkoutsContext } from './useWorkoutsContext';
 
 //to log out 1) update local state 2) delete JWT from local storage
 export const useLogout = () => {
-  const { dispatch } = useAuthContext();
+  const { dispatch: authDispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
   const logout = () => {
     //remove user from storage
     localStorage.removeItem('user');
 
     //dispatch logout action
-    dispatch({ type: 'LOGOUT' });
+    authDispatch({ type: 'LOGOUT' });
+    workoutsDispatch({ type: 'SET_WORKOUTS', payload: null });
 
     console.log('User is Logged out');
   };
